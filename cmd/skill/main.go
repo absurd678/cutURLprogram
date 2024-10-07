9021828a4c70601c1929b7571e10be7a6e138483
Artem <artemykozhevnik@gmail.com>
2024-10-07 17:29:51 +0300

    The 1st increment is done

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -6,40 +6,32 @@ import (
 	"strings"
 )
 
-func postEndpoint(res http.ResponseWriter, req *http.Request) {
+var validPathget = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
+var validPathpost = regexp.MustCompile("^/$")
+
+func Endpoint(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
+		if m := validPathpost.FindStringSubmatch(req.URL.Path); m == nil {
+			http.Error(res, "Invalid URL for post", http.StatusBadRequest)
+			return
+		}
 		res.WriteHeader(http.StatusCreated)
 		urlBody := strings.Join(req.Header.Values("Content-Type"), " ")
 		res.Header().Add("Content-Type", urlBody)
-	}
-}
-
-func getEndpoint(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		res.WriteHeader(http.StatusTemporaryRedirect)
-		res.Header().Add("Location", req.URL.String())
-	} else {
-		http.Redirect(res, req, req.URL.String(), http.StatusCreated)
-	}
-}
-
-var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.Error(w, "Bad URL path", http.StatusBadRequest)
+	} else if req.Method == http.MethodGet {
+		if m := validPathget.FindStringSubmatch(req.URL.Path); m == nil {
+			http.Error(res, "Invalid URL for get", http.StatusBadRequest)
 			return
 		}
-		fn(w, r)
+		res.WriteHeader(http.StatusTemporaryRedirect)
+		res.Header().Add("Location", req.URL.String())
 	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", makeHandler(postEndpoint))
-	mux.HandleFunc("/get", makeHandler(getEndpoint))
+	mux.HandleFunc("/", Endpoint)
+
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
